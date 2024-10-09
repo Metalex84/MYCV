0bf03c0bf0b03a79e9e08d0de6799c9d0bd3c4c3
AGV <agonzalez.venegas@outlook.com>
2024-10-09 18:55:11 +0200

    Cambio colores Toggle Dark Mode Switch

--- js/main.js
+++ js/main.js
@@ -10,9 +10,9 @@ toggleButton.addEventListener('click', function() {
 
     // Cambia el botón dependiendo del modo actual
     if (body.classList.contains('dark-mode')) {
-        toggleButton.innerHTML = '<div class="form-check form-switch"><input class="form-check-input" type="checkbox" id="toggle-dark-mode" checked><label class="form-check-label" for="toggle-dark-mode"><p class="page-subtitle">Modo claro</p></label></div>';
+        toggleButton.innerHTML = '<div class="form-check form-switch"><input class="form-check-input" type="checkbox" id="toggle-dark-mode" checked><label class="form-check-label" for="toggle-dark-mode"><p class="page-subtitle">Modo oscuro</p></label></div>';
     } else {
-        toggleButton.innerHTML = '<div class="form-check form-switch"><input class="form-check-input" type="checkbox" id="toggle-dark-mode"><label class="form-check-label" for="toggle-dark-mode"><p class="page-subtitle.dark-mode">Modo oscuro</p></label></div>';
+        toggleButton.innerHTML = '<div class="form-check form-switch"><input class="form-check-input" type="checkbox" id="toggle-dark-mode"><label class="form-check-label" for="toggle-dark-mode"><p class="page-subtitle.dark-mode">Modo claro</p></label></div>';
     }
 
     const pageTitle = document.querySelector('.page-title');
